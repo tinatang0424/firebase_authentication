5d197f12363230996fff566368fb6f60712a2404
tinatang0424 <tinatang0424@gmail.com>
2022-06-28 21:09:53 +0800

    Update commit

--- app/src/main/java/com/tinatang0424/app_project/firebase_auth/RegisterActivity.kt
+++ app/src/main/java/com/tinatang0424/app_project/firebase_auth/RegisterActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_password.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -29,24 +30,30 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun register() {
         register_btn.setOnClickListener {
-            if(TextUtils.isEmpty(email_edt.text.toString())) {
+            val email_str = email_edt.text.toString()
+            val password_str = password_edt.text.toString()
+
+            if(TextUtils.isEmpty(email_str)) {
                 Toast.makeText(this@RegisterActivity, "請輸入email", Toast.LENGTH_SHORT).show()
             }
-            else if(TextUtils.isEmpty(password_edt.text.toString())) {
+            else if(TextUtils.isEmpty(password_str)) {
                 Toast.makeText(this@RegisterActivity, "請輸入密碼", Toast.LENGTH_SHORT).show()
+            }
+            else if(password_str.length<6) {
+                Toast.makeText(this@RegisterActivity, "密碼長度需大於5", Toast.LENGTH_SHORT).show()
             }
             else {
                 auth.createUserWithEmailAndPassword(
-                    email_edt.text.toString(),
-                    password_edt.text.toString()
+                    email_str,
+                    password_str
                 )
                     .addOnCompleteListener {
                         if (it.isSuccessful) {
                             val currentUser = auth.currentUser
                             val currentUserDb = databaseReference?.child((currentUser?.uid!!))
 
-                            currentUserDb?.child("email")?.setValue(email_edt.text.toString())
-                            currentUserDb?.child("password")?.setValue(password_edt.text.toString())
+                            currentUserDb?.child("email")?.setValue(email_str)
+                            currentUserDb?.child("password")?.setValue(password_str)
 
                             Toast.makeText(this@RegisterActivity, "註冊成功!", Toast.LENGTH_LONG).show()
                             startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
